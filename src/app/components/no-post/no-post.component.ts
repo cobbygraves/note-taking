1c3c64ea1341b0b6d1ef1057e20149231b374c57
cobbygraves <rmeocnis@hotmail.com>
2025-06-24 17:51:24 +0000

    remove no-notes link

--- src/app/components/no-post/no-post.component.ts
+++ src/app/components/no-post/no-post.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-no-post',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './no-post.component.html',
   styleUrl: './no-post.component.scss',
 })
