fc474843b1fb06762e997bfc85303faa404483b5
cobbygraves <rmeocnis@hotmail.com>
2025-06-26 20:45:18 +0000

    feat:light and dark theme toggle

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { UserService } from '../../services/user.service';
-import { FormsModule, NgForm } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { NoteService } from '../../services/note.service';
+import { ToggleSwitch } from 'primeng/toggleswitch';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, FormsModule, ToggleSwitch],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   term: string = '';
+  checked: boolean = true;
   constructor(
     public userService: UserService,
     private noteService: NoteService
